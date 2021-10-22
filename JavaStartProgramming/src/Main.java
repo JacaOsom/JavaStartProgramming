276362e6b59088e15760fdf1769325db4d70e1b4
Jacek Filipski 56701 <jacekfilipski@gmail.com>
2021-10-22 21:47:32 +0200

    Exercise_2

--- JavaStartProgramming/src/Main.java
+++ JavaStartProgramming/src/Main.java
@@ -5,12 +5,28 @@ public class Main
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        double F, C;
+        /*double F, C;
         System.out.println("Please enter temperature in Fahrenheit's degrees");
         F = scanner.nextDouble();
         C = (double)5 / 9 * (F-32);
-        System.out.println(C);
+        System.out.println(C);*/
 
+//        final int PCs = 24;
+//        int students;
+//        double result;
+//        System.out.printf("Enter the number of students: ");
+//        students = scanner.nextInt();
+//        result = (double)students / PCs;
+//        System.out.println("The result is: " + result);
+
+        double age, money;
+        boolean cinema;
+        System.out.printf("Enter the age: ");
+        age = scanner.nextDouble();
+        System.out.printf("How much money do you have?: ");
+        money = scanner.nextDouble();
+        cinema = (age >= 18 && money >= 20);
+        System.out.println("The result is: " + cinema);
 
 
 
