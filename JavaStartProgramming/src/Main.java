26c95355676af3f382ec1c99df726fab43be3a3e
Jacek Filipski 56701 <jacekfilipski@gmail.com>
2021-10-22 21:33:27 +0200

    Exercise_2

--- JavaStartProgramming/src/Main.java
+++ JavaStartProgramming/src/Main.java
@@ -1,8 +1,20 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
+        double F, C;
+        System.out.println("Please enter temperature in Fahrenheit's degrees");
+        F = scanner.nextDouble();
+        C = (double)5 / 9 * (F-32);
+        System.out.println(C);
+
+
+
+
+
 
 
     }
