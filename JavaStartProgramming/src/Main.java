9a5442b4cf8748c4f28932637d268dcf185e84ca
Jacek Filipski 56701 <jacekfilipski@gmail.com>
2021-10-22 22:20:49 +0200

    Simple exercises

--- JavaStartProgramming/src/Main.java
+++ JavaStartProgramming/src/Main.java
@@ -5,7 +5,8 @@ public class Main
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        //2.1
+//        2.1
+        System.out.println("Exercise 2.1");
         double celsius, fahrenheit;
         System.out.print("Enter the temperature in Celsius's degrees: ");
         celsius = scanner.nextDouble();
@@ -15,5 +16,32 @@ public class Main
         fahrenheit = Math.round(fahrenheit);
         fahrenheit /= round;
         System.out.println("The temperature in Fahrenheit degrees is: " + fahrenheit);
+        System.out.println("");
+
+//        2.2
+        System.out.println("Exercise 2.2");
+        System.out.print("Enter the coefficient a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Enter the coefficient b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Enter the coefficient c: ");
+        double c = scanner.nextDouble();
+
+        double delta = Math.pow(b, 2) - (4 * a * c);
+        System.out.println("The result of delta is: " + delta);
+        System.out.println("");
+
+//        2.3
+        System.out.print("Enter your weight (in kilogram): ");
+        double weight = scanner.nextDouble();
+        System.out.print("Enter your height (in meters): ");
+        double height = scanner.nextDouble();
+        double BMI = weight / Math.pow(height, 2);
+
+        System.out.println("The result of Your BMI is: " + BMI);
+        System.out.println("");
+
+
+
     }
 }
