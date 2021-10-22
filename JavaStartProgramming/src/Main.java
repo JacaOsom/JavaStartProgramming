7a97c0f48b5f83efa91535f8e396223878f15cc9
Jacek Filipski 56701 <jacekfilipski@gmail.com>
2021-10-22 14:19:21 +0200

    Next lesson

--- JavaStartProgramming/src/Main.java
+++ JavaStartProgramming/src/Main.java
@@ -4,17 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String firstName, lastName;
-        int age;
-        System.out.println("Hello user! Please, enter your first name, last name and age!");
-        System.out.print("First name: ");
-        firstName = scanner.next();
-        System.out.print("Last name: ");
-        lastName = scanner.next();
-        System.out.print("Age: ");
-        age = scanner.nextInt();
 
-        System.out.println("Hello " + firstName + " " + lastName + "!");
-        System.out.println("Your age is: " + age);
     }
 }
